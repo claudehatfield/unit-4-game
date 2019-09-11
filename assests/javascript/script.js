69675b9725fbbfe0eb6931de928f66a7e212d6b0
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-10 23:04:41 -0500

    finished javascript logic

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,45 +1,104 @@
 $(document).ready(() => {
 
   var totalScore = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-  var crystalOne= Math.floor(Math.random() * 12) + 1;
+  var crystalOne = Math.floor(Math.random() * 12) + 1;
   var crystalTwo = Math.floor(Math.random() * 12) + 1;
   var crystalThree = Math.floor(Math.random() * 12) + 1;
   var crystalFour = Math.floor(Math.random() * 12) + 1;
   var wins = 0;
   var lose = 0;
-  var playerScore= 0;
-
-  
-//  Generates computer score to match
-$(".totalScore").html(totalScore);
-
-// Players current score
-
-$(".playerScore").text( playerScore);
+  var playerScore = 0;
 
 
 
-// adds crystal score to player score
-$(".crystalOne").click (function (){
-  playerScore = playerScore + crystalOne
-  $('.playerScore').text(playerScore);
-});
+  //  Generates computer score to match
+  $(".totalScore").html(totalScore);
 
-$(".crystalTwo").click (function (){
-  playerScore = playerScore + crystalTwo
-  $('.playerScore').text(playerScore);
-});
+  // Players current score
 
-$(".crystalThree").click (function (){
-  playerScore = playerScore + crystalThree
-  $('.playerScore').text(playerScore);
-});
+  $(".playerScore").text(playerScore);
 
-$(".crystalFour").click (function (){
-  playerScore = playerScore + crystalFour
-  $('.playerScore').text(playerScore);
-});
+  // win condition
 
-console.log(crystalOne, crystalTwo, crystalThree, crystalFour);
+  function win() {
+    wins++
+    $(".win").text(wins)
+  }
+  // loss condition
+  function loss() {
+    lose++
+    $(".loss").text(lose);
+  }
+
+
+
+  // Asses rather wins or loses
+
+  function outcome() {
+    if (playerScore === totalScore) {
+      win();
+      start();
+      reset();
+
+
+    } else if (playerScore > totalScore) {
+      loss();
+      start();
+      reset();
+
+
+    }
+  }
+
+
+  // adds crystal score to player score
+  $(".crystalOne").click(function () {
+    playerScore = playerScore + crystalOne
+    $('.playerScore').text(playerScore);
+    outcome();
+
+  });
+
+  $(".crystalTwo").click(function () {
+    playerScore = playerScore + crystalTwo
+    $('.playerScore').text(playerScore);
+    outcome();
+
+  });
+
+  $(".crystalThree").click(function () {
+    playerScore = playerScore + crystalThree
+    $('.playerScore').text(playerScore);
+    outcome();
+
+  });
+
+  $(".crystalFour").click(function () {
+    playerScore = playerScore + crystalFour
+    $('.playerScore').text(playerScore);
+    outcome();
+
+  });
+
+  console.log(crystalOne, crystalTwo, crystalThree, crystalFour);
+
+  // Reset the game
+  function reset() {
+    playerScore = 0;
+
+    $(".playerScore").text(playerScore);
+
+  }
+
+  function start() {
+    totalScore = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    crystalOne = Math.floor(Math.random() * 12) + 1;
+    crystalTwo = Math.floor(Math.random() * 12) + 1;
+    crystalThree = Math.floor(Math.random() * 12) + 1;
+    crystalFour = Math.floor(Math.random() * 12) + 1;
+    $(".totalScore").html(totalScore);
+    console.log(crystalOne, crystalTwo, crystalThree, crystalFour);
+  }
+
 
 })
